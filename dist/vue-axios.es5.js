297a59df51d427bf63f9a08e6678a1d88d0b2bb6
imcvampire <quocanh96@gmail.com>
2016-10-25 15:16:38 +0700

    style:

--- dist/vue-axios.es5.js
+++ dist/vue-axios.es5.js
@@ -4,13 +4,74 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 var _axios = require('axios');
 
 var _axios2 = _interopRequireDefault(_axios);
 
-var _utils = require('./utils');
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ *  Copied from vue-resource
+ */
+
+function isFunction(val) {
+  return typeof val === 'function';
+}
+
+var isArray = Array.isArray;
+
+function isPlainObject(obj) {
+  return isObject(obj) && Object.getPrototypeOf(obj) == Object.prototype;
+}
+
+function isObject(obj) {
+  return obj !== null && (typeof obj === 'undefined' ? 'undefined' : _typeof(obj)) === 'object';
+}
+
+function _merge(target, source, deep) {
+  for (var key in source) {
+    if (deep && (isPlainObject(source[key]) || isArray(source[key]))) {
+      if (isPlainObject(source[key]) && !isPlainObject(target[key])) {
+        target[key] = {};
+      }
+      if (isArray(source[key]) && !isArray(target[key])) {
+        target[key] = [];
+      }
+      _merge(target[key], source[key], deep);
+    } else if (source[key] !== undefined) {
+      target[key] = source[key];
+    }
+  }
+}
+
+function merge(target) {
+
+  var args = slice.call(arguments, 1);
+
+  args.forEach(function (source) {
+    _merge(target, source, true);
+  });
+
+  return target;
+}
+
+function options(fn, obj, opts) {
+
+  opts = opts || {};
+
+  if (isFunction(opts)) {
+    opts = opts.call(obj);
+  }
+
+  return merge(fn.bind({ $vm: obj, $options: opts }), fn, { $options: opts });
+}
+
+/**
+ * Install plugin
+ * @param Vue
+ */
 
 function plugin(Vue) {
 
@@ -24,7 +85,7 @@ function plugin(Vue) {
 
     axios: {
       get: function get() {
-        return (0, _utils.options)(Vue.axios, this, this.$options.axios);
+        return options(Vue.axios, this, this.$options.axios);
       }
     }
 
