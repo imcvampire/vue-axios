b281031dcbc22487278a6e07d86a5c5e5f68368b
imcvampire <quocanh96@gmail.com>
2016-10-25 14:54:43 +0700

    build: add gulp

---
+++ Gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglifyjs');
+const rename = require('gulp-rename');
+
+gulp.task('default', () => {
+    return gulp.src('src/index.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(rename('vue-axios.es5.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(uglify())
+        .pipe(rename('vue-axios.min.js'))
+        .pipe(gulp.dest('dist'));
+});
