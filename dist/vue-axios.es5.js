a009e850e410db9d02752793e637ead46fe8e086
imcvampire <quocanh96@gmail.com>
2017-03-10 21:14:44 +0700

    feat: bump version

--- dist/vue-axios.es5.js
+++ dist/vue-axios.es5.js
@@ -1,67 +1,8 @@
-'use strict';
+"use strict";
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 (function () {
-
-  /**
-   *  Copied from vue-resource
-   */
-
-  var slice = [].slice;
-
-
-  function isFunction(val) {
-    return typeof val === 'function';
-  }
-
-  var isArray = Array.isArray;
-
-  function isPlainObject(obj) {
-    return isObject(obj) && Object.getPrototypeOf(obj) == Object.prototype;
-  }
-
-  function isObject(obj) {
-    return obj !== null && (typeof obj === 'undefined' ? 'undefined' : _typeof(obj)) === 'object';
-  }
-
-  function _merge(target, source, deep) {
-    for (var key in source) {
-      if (deep && (isPlainObject(source[key]) || isArray(source[key]))) {
-        if (isPlainObject(source[key]) && !isPlainObject(target[key])) {
-          target[key] = {};
-        }
-        if (isArray(source[key]) && !isArray(target[key])) {
-          target[key] = [];
-        }
-        _merge(target[key], source[key], deep);
-      } else if (source[key] !== undefined) {
-        target[key] = source[key];
-      }
-    }
-  }
-
-  function merge(target) {
-
-    var args = slice.call(arguments, 1);
-
-    args.forEach(function (source) {
-      _merge(target, source, true);
-    });
-
-    return target;
-  }
-
-  function options(fn, obj, opts) {
-
-    opts = opts || {};
-
-    if (isFunction(opts)) {
-      opts = opts.call(obj);
-    }
-
-    return merge(fn.bind({ $vm: obj, $options: opts }), fn, { $options: opts });
-  }
 
   /**
    * Install plugin
@@ -86,14 +27,20 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
       axios: {
         get: function get() {
-          return options(Vue.axios, this, this.$options.axios);
+          return axios;
+        }
+      },
+
+      $http: {
+        get: function get() {
+          return axios;
         }
       }
 
     });
   }
 
-  if ((typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) == "object") {
+  if ((typeof exports === "undefined" ? "undefined" : _typeof(exports)) == "object") {
     module.exports = plugin;
   } else if (typeof define == "function" && define.amd) {
     define([], function () {
