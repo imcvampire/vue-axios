da5c5a0fbaf9a601bfd47be38b991cf838113531
小凡 <718662552@qq.com>
2020-09-29 15:00:27 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,6 @@
 import { AxiosStatic } from "axios";
 import { App } from "vue";
 
-export declare function VAxios(app: App): AxiosStatic;
 declare module "@vue/runtime-core" {
   export interface ComponentCustomProperties {
     $http: AxiosStatic;
