47e543edc9575f7aa7688728133e8f2bcd28d865
Quoc-Anh Nguyen <anh.nq@teko.vn>
2020-10-14 09:27:56 +0700

    feat(*): add rollup to bundle js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,27 +1,72 @@
-const { src, dest, series } = require('gulp');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglifyjs');
+// Gulp
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const file = require('gulp-file');
+const filter = require('gulp-filter');
 const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require ('gulp-uglify');
 const clean = require('gulp-clean');
+const commonjs = require('@rollup/plugin-commonjs')
 
-function build() {
-  return src('src/index.js')
-    .pipe(babel({
-      presets: [
-        '@babel/env'
-      ]
-    }))
-    .pipe(rename('vue-axios.es5.js'))
-    .pipe(dest('dist'))
-    .pipe(uglify())
-    .pipe(rename('vue-axios.min.js'))
-    .pipe(dest('dist'));
+// Rollup
+const { rollup } = require('rollup');
+const babel = require('rollup-plugin-babel');
+const { nodeResolve }= require('@rollup/plugin-node-resolve')
+
+// Misc
+const runSequence = require('run-sequence');
+
+// Const
+const buildPath = 'dist/';
+
+function _generate(bundle){
+  return bundle.generate({
+    format: 'commonjs',
+  });
 }
 
-function clear() {
-  return src('dist/*').pipe(clean({
+function bundle(opts) {
+  return rollup({
+    input: 'src/index.js',
+    plugins: [
+      nodeResolve({ browser: true }),
+      commonjs(),
+      babel({
+        presets: [
+          ["@babel/env"]
+        ],
+        sourceMaps: true
+      })
+    ]
+  }).then(bundle => {
+    return _generate(bundle);
+  });
+}
+
+gulp.task('build', function(){
+  return bundle().then(gen => {
+    return file('vue-axios.es5.js', gen.code, {src: true})
+      // .pipe(plumber())
+      // .pipe(sourcemaps.init({loadMaps: true}))
+      // .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest(buildPath))
+      // .pipe(filter(['*', '!**/*.js.map']))
+      // .pipe(rename('vue-axios.min.js'))
+      // .pipe(sourcemaps.init({loadMaps: true}))
+      // .pipe(uglify({
+        // preserveComments: 'license'
+      // }))
+      // .pipe(sourcemaps.write('./'))
+      // .pipe(gulp.dest(buildPath));
+  });
+});
+
+gulp.task('clean', function() {
+  return gulp.src('dist/*').pipe(clean({
     force: true
   }));
-}
+})
 
-exports.default = series(clear, build)
+gulp.task('default', gulp.series('clean', 'build'))
+
