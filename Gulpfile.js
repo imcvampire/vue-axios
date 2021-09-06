2ef1d8a2f6f9068bc086bdaf4ec04ffaf6b08511
KudouSterain <hotrungnhan29@gmail.com>
2021-09-06 14:55:13 +0700

    Remove some no need package ,  update old package , edit grulp build file, add build bundle test

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,71 +1,70 @@
 // Gulp
-const gulp = require('gulp');
-const plumber = require('gulp-plumber');
-const file = require('gulp-file');
-const filter = require('gulp-filter');
-const merge = require('merge-stream');
-const uglify = require('gulp-uglify');
-const clean = require('gulp-clean');
-const commonjs = require('@rollup/plugin-commonjs')
+const gulp = require('gulp')
+const plumber = require('gulp-plumber')
+const file = require('gulp-file')
+const filter = require('gulp-filter')
+const uglify = require('gulp-uglify')
+const clean = require('gulp-clean')
 
 // Rollup
-const { rollup } = require('rollup');
-const babel = require('rollup-plugin-babel');
+const { rollup } = require('rollup')
 const { nodeResolve } = require('@rollup/plugin-node-resolve')
-
+const { getBabelOutputPlugin } = require('@rollup/plugin-babel')
 // Const
-const buildPath = 'dist/';
+const buildPath = 'dist/'
 
 /**
  * Generate scripts with commonjs module
  * @param {import('rollup').RollupBuild} bundle
  */
-function bundleCommonJs(bundle) {
-  return bundle.generate({
-    format: 'umd',
-    name: 'VueAxios'
-  });
-}
 
 /**
  * Bundle index.js using rollup + babel
  */
-async function bundle() {
+async function bundle(name, format) {
   const bundle = await rollup({
     input: 'src/index.js',
     plugins: [
       nodeResolve({ browser: true }),
-      commonjs(),
-      babel({
-        presets: [
-          ["@babel/env"]
-        ],
-        sourceMaps: true
-      })
-    ]
-  });
-  return bundleCommonJs(bundle);
+      getBabelOutputPlugin({
+        allowAllFormats: true,
+        presets: [['@babel/preset-env', { modules: 'auto' }]],
+        // sourceMaps: true,
+      }),
+    ],
+  })
+  return bundle.generate({
+    format: format,
+    name: name,
+  })
 }
 
 gulp.task('build', async function () {
-  const generatedBundle = await bundle();
-  const f = filter(['*', '!**/*.js.map',], { restore: true });
-  const data = ['vue-axios.es5.js', 'vue-axios.min.js'];
-  const streams = data.map((name) => {
-    return file(name, generatedBundle.output.map(o => o.code).join(" "), { src: true })
-    .pipe(plumber())
-    .pipe(f)
-    .pipe(uglify())
-    .pipe(gulp.dest(buildPath));
-  });
+  const esmbundle = await bundle('VueAxios', 'esm')
+  const commonbundle = await bundle('VueAxios', 'umd')
+  const f = filter(['*', '!**/*.js.map'], { restore: true })
+  const data = {
+    'vue-axios.common.min.js': commonbundle,
+    'vue-axios.esm.min.js': esmbundle,
+  }
 
-  return merge(streams);
-});
+  for (const [name, bundle] of Object.entries(data)) {
+    file(name, bundle.output.map((o) => o.code).join(' '), {
+      src: true,
+    })
+      .pipe(plumber())
+      .pipe(f)
+      .pipe(uglify())
+      .pipe(gulp.dest(buildPath))
+  }
+})
 
 gulp.task('clean', function () {
-  return gulp.src('dist/*').pipe(clean({
-    force: true
-  }));
+  return gulp.src('dist/*').pipe(
+    clean({
+      force: true,
+    }),
+  )
 })
 
 gulp.task('default', gulp.series('clean', 'build'))
